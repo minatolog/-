9f4e8724f4d36a0a2be3770dc1f65f86f8903475
luogewen <z5581485@ad.unsw.edu.au>
2026-04-12 22:49:22 +1000

    route structure building

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,6 +1,10 @@
 import { useState } from 'react'
 import { BrowserRouter, Routes,Route, Navigate } from 'react-router-dom'
 import AuthContainer from './auth/AuthContainer';
+import LoginPage from './auth/LoginPage';
+import ReisterPage from './auth/ReigsterPage'; 
+import LandingPage from './auth/LandingPage';
+import Dashboard from './dashboard/dashboard';
 function App() {
   const[token,setTokenState]=useState(localStorage.getItem('token'));
   return (
@@ -14,10 +18,14 @@ function App() {
        }>   
        </Route>
          
+      <Route index element={<LandingPage />} />
+      <Route path="login" element={<LoginPage />} />
+      <Route path="register" element={<ReisterPage />} />
+      <Route path='dashboard'
+       element={token? <Dashboard /> : < Navigate to ="/auth" replace />}
       
+      />
      </Routes>
-     
-     
      </BrowserRouter>
 
   )
---
+++ frontend/src/auth/ReigsterPage.tsx
@@ -0,0 +1,3 @@
+export default function ReisterPage() {
+  return <div>register page</div>;
+}
