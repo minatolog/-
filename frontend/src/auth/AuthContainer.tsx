d49a89c9924a8a99bf7b0aa3d9d727ac7fe524dd
luogewen <z5581485@ad.unsw.edu.au>
2026-04-13 16:20:08 +1000

    landing page set

--- frontend/src/auth/AuthContainer.tsx
+++ frontend/src/auth/AuthContainer.tsx
@@ -1,11 +1,27 @@
-import { Outlet } from 'react-router-dom';
+import { Outlet } from 'react-router-dom'
+import { Box, Paper } from '@mui/material'
 
 export default function AuthContainer() {
-  
   return (
-    <>
-    <div>auth container</div>
-  <Outlet />
-   </>
+    <Box
+      sx={{
+        minHeight: '100vh',
+        display: 'flex',
+        alignItems: 'center',
+        justifyContent: 'center',
+        bgcolor: '#fbfbfb',
+      }}
+    >
+      <Paper
+        elevation={3}
+        sx={{
+          p: 4,
+          width: 350,
+          textAlign: 'center',
+        }}
+      >
+        <Outlet />
+      </Paper>
+    </Box>
   )
 }
