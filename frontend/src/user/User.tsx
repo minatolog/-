899834844aaf93820028e89c7546c78b037d7f4d
luogewen <z5581485@ad.unsw.edu.au>
2026-04-17 17:26:43 +1000

    delete a useless button

--- frontend/src/user/User.tsx
+++ frontend/src/user/User.tsx
@@ -1,18 +1,8 @@
 import { Outlet } from 'react-router-dom';
-import { useContext } from 'react';
-import { Button } from '@mui/material';
-import { AuthContext } from '../auth/AuthContext';
 
 export default function User() {
-  const { setToken } = useContext(AuthContext);
-
   return (
     <>
-
-      <Button onClick={() => setToken(null)}>
-        Logout
-      </Button>
-
       <Outlet />
     </>
   );
--- frontend/src/user/dashboard/Dashboard.tsx
+++ frontend/src/user/dashboard/Dashboard.tsx
@@ -1,7 +1,7 @@
 import { useContext } from "react";
 import { AuthContext } from "../../auth/AuthContext.tsx";
 import { Box, Button } from "@mui/material";
-import Header from "./Header.tsx";
+import Header from "./Header.tsx";  
 
 export default function Dashboard() {
   const { setToken } = useContext(AuthContext);
