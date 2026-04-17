6e8f97618d6806048e8464548ce79d0450214da2
luogewen <z5581485@ad.unsw.edu.au>
2026-04-18 01:42:26 +1000

    new presentation dialog, preview area and preview card

--- frontend/src/user/dashboard/NewPresentationDialog.tsx
+++ frontend/src/user/dashboard/NewPresentationDialog.tsx
@@ -1,4 +1,20 @@
-export default function NewPresentationDialog() {
+import type { PresentationType } from "../PresentaionType.ts";
+
+type NewPresentationDialogProps = {
+  onClose: () => void;
+  onCreated: (_newPresentation: PresentationType) => void | Promise<void>;
+  onError: (_message: string) => void;
+};
+
+export default function NewPresentationDialog({
+  onClose,
+  onCreated,
+  onError,
+}: NewPresentationDialogProps) {
+  void onClose;
+  void onCreated;
+  void onError;
+
   return (
     <div>
       New Presentation Dialog
