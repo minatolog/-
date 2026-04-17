6707a285710c718843700677195e92d3f6266470
luogewen <z5581485@ad.unsw.edu.au>
2026-04-17 21:53:39 +1000

    loading list aand upadtelist

--- frontend/src/user/User.tsx
+++ frontend/src/user/User.tsx
@@ -1,6 +1,8 @@
 import { Outlet } from 'react-router-dom';
 import type { PresentationType } from './PresentaionType';
 import { useState,useRef } from 'react';
+import { getPresentationList } from '../api';
+
 export default function User() {
   const [presentationist, setPresentationist] = useState<PresentationType[]>([]);
   const presentationistRef = useRef<PresentationType[]>(presentationist);
@@ -11,6 +13,35 @@ export default function User() {
     presentationistRef.current = presentationList;
     setPresentationist(presentationList);
   }
+    async function loadPresentationlist() {
+    setLoading(true);
+    try {
+      const list = await getPresentationList();
+      setPresentationList(list);
+    } catch (error) {
+      setErrorMessage(
+        error instanceof Error
+          ? error.message
+          : 'Failed to load presentations.'
+      );
+    } finally {
+      setLoading(false);
+    }
+  }
+
+ async function updateList(newList: PresentationType[]) {
+    try {
+      await UpdatePresentationList(newList);
+      setPresentationList(newList);
+    } catch {
+      throw Error('failed to save');
+    }
+  }
+
+
+
+
+
 
   return (
     <>
